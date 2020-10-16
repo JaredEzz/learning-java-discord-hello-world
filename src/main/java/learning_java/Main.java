03b47a03ec17e03f4496fce6ee40b17a0c8d8bad
JaredEzz <jaredezzethasson@gmail.com>
2020-10-16 01:13:22 -0600

    test pipeline with minimal updates

--- src/main/java/learning_java/Main.java
+++ src/main/java/learning_java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         String discordUserName = "JaredEzz";
         System.out.printf("Hi! I'm %s, and I want to help others learn Java. Please introduce yourself!%n" +
-                "Feel free to update the variable above, and use it, and add any other code you'd like!%n", discordUserName);
+                "Feel free to update the username variable above and use it, and add any other code you'd like!%n", discordUserName);
     }
 }
