8f676f22c9dbc7e7290fffa1e916df4e3d44ad12
JaredEzz <jaredezzethasson@gmail.com>
2020-10-16 00:30:21 -0600

    add intro for JaredEzz

---
+++ src/learning_java/Main.java
@@ -0,0 +1,10 @@
+package learning_java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String discordUserName = "JaredEzz";
+        System.out.printf("Hi! I'm %s, and I want to help others learn Java. Please introduce yourself!%n" +
+                "Feel free to update the variable above, and use it, and add any other code you'd like!%n", discordUserName);
+    }
+}
--- src/me/jaredhasson/Main.java
+++
@@ -1,8 +0,0 @@
-package me.jaredhasson;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
